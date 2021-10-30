e393b0698f4c8ed66b5b56188db5a9a3cd0c2b9e
nmso2 <nmsubho@gmail.com>
2021-10-31 01:00:25 +0600

    update

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function run() {
         // DELETE API
         app.delete('/purchasePlan/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectID(id) };
+            const query = { _id: ObjectId(id) };
             const result = await purchaseCollection.deleteOne(query);
             res.json(result);
         })
